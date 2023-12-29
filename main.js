d331b9e70ae746976d2d6095df2c4c01183bc6b8
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-12-29 08:44:23 +0800

    fix bugs

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ function updateCountdown(d) {
   var h = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var m = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
   var s = Math.floor((t % (1000 * 60)) / 1000);
+  var ss = Math.floor((t / (1000 * 60)) / 1000);
   
-  if (t < 2) {
+  if (s < 1 && m < 1 && h < 1 && d < 1 ) {
     document.getElementById("year").style.animation = "colorfade 2.5s infinite alternate linear";
     document.getElementById("label").innerHTML = "Hope this year will be a great year!!";
     document.getElementById("bg").style.backgroundColor = "#000";
@@ -18,11 +19,18 @@ function updateCountdown(d) {
     return "HAPPY NEW YEAR 2024!!";
   }
   
+  if (s < 59 && m < 1 && h < 1 && d < 1) {
+    document.getElementById("bg").style.backgroundColor = "#001";
+  }
+  if (s < 30 && m < 1 && h < 1 && d < 1) {
+    document.getElementById("bg").style.backgroundColor = "#000";
+  }
+  
   return d + " days, " + h + " hours, " + m + " minutes, and " + s + " seconds left";
 }
 ;
 window.onload = function() {
   setInterval(() => {
-    document.getElementById("countdown").innerHTML = updateCountdown("Jan 1 2024 00:00:00 GMT-500");
+    document.getElementById("countdown").innerHTML = updateCountdown("Jan 1, 2024 00:00:00 GMT-500");
   })
 }
